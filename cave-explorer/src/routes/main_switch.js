26599ee17170248471783b3e31232fd81bd1e412
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-11-15 22:44:03 -0800

    :sparkles: Authentication
    Authentication is built, both login and register tested for functionality and are working based on coms with the Lambda Test Server

--- cave-explorer/src/routes/main_switch.js
+++ cave-explorer/src/routes/main_switch.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Login from "../views/LoginPage/login";
-// import Register from "../view/Register";
+import Register from "../views/RegisterPage/Register";
 // import Game from "../view/Game";
 import PrivateRoute from "./PrivateRoute";
 
 const appRoutes = () => (
   <Switch>
     <Route path="/login" component={Login} />
-    {/* <Route path="/signup" component={Register} /> */}
-    {/* <Route path="/" component={Game} />
-    <Route render={() => <Redirect to="/" />} /> */}
+    <Route path="/signup" component={Register} />
+    {/* <Route path="/" component={Game} /> */}
+    <Route render={() => <Redirect to="/" />} />
   </Switch>
 );
 
