e30996bbf4cffdb4e5e7d15636e59b6ae24799d9
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-11-15 22:55:52 -0800

    :sparkles: Authentication
    Finished off and tested authentication for login and register Just going to start styling now

--- cave-explorer/src/routes/main_switch.js
+++ cave-explorer/src/routes/main_switch.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Login from "../views/LoginPage/login";
 import Register from "../views/RegisterPage/Register";
-// import Game from "../view/Game";
+import Game from "../views/GamePage/Game";
 import PrivateRoute from "./PrivateRoute";
 
 const appRoutes = () => (
   <Switch>
     <Route path="/login" component={Login} />
     <Route path="/signup" component={Register} />
-    {/* <Route path="/" component={Game} /> */}
+    <Route path="/" component={Game} /> /* This will be Replaced with privateroute when the time comes.*/
     <Route render={() => <Redirect to="/" />} />
   </Switch>
 );
